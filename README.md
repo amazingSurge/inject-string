0241343fc6b40a4e386e9668e754b91825140205
amazingSurge <amazingSurge@yahoo.com>
2016-08-16 22:59:04 +0800

    update Readme

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # inject-string [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+
 > Inject a snippet of code or content into a string.
 
 ## Installation
+
+Install with [npm](https://www.npmjs.com/)
 
 ```sh
 $ npm install --save inject-string
@@ -10,14 +13,149 @@ $ npm install --save inject-string
 ## Usage
 
 ```js
-var injectString = require('inject-string');
+var InjectString = require('inject-string');
 
-injectString('Rainbow');
+var inject = new InjectString('before <!-- snippet --> after');
+inject.append('foo');
+//=> 'before <!-- snippet -->\nfoo\n<!-- endsnippet --> after'
 ```
+
+## Actions
+###append
+
+```js
+var inject = new InjectString('before <!-- snippet -->foo<!-- endsnippet --> after');
+inject.append('bar');
+//=> 'before <!-- snippet -->\nfoobar\n<!-- endsnippet --> after'
+```
+
+### prepend
+
+```js
+var inject = new InjectString('before <!-- snippet -->foo<!-- endsnippet --> after');
+inject.prepend('bar');
+//=> 'before <!-- snippet -->\nbarfoo\n<!-- endsnippet --> after'
+```
+
+### replace
+
+```js
+var inject = new InjectString('before <!-- snippet -->foo<!-- endsnippet --> after');
+inject.replace('bar');
+//=> 'before <!-- snippet -->\nbar\n<!-- endsnippet --> after'
+```
+
+### strip
+
+```js
+var inject = new InjectString('before <!-- snippet -->foo<!-- endsnippet --> <!-- custom -->bar<!-- endcustom --> after');
+inject.strip('custom');
+//=> 'before <!-- snippet -->foo<!-- endsnippet --> bar after'
+```
+
+### stripAll
+
+```js
+var inject = new InjectString('before <!-- snippet -->foo<!-- endsnippet --> <!-- custom -->bar<!-- endcustom --> after');
+inject.stripAll();
+//=> 'before foo bar after'
+```
+
+## Static call
+
+### inject
+
+```js
+InjectString.inject('a <!-- snippet --> b', 'foo', {stripTags: true});
+//=> 'a foo b'
+```
+
+### strip
+
+```js
+InjectString.strip('before <!-- snippet -->foo<!-- endsnippet --> <!-- custom -->bar<!-- endcustom --> after', {tag:'custom'});
+//=> 'before <!-- snippet -->foo<!-- endsnippet --> bar after'
+```
+
+### stripAll
+
+```js
+InjectString.stripAll('before <!-- snippet -->foo<!-- endsnippet --> <!-- custom -->bar<!-- endcustom --> after');
+//=> 'before foo bar after'
+```
+
+## Options
+### Keep placeholders
+
+Inject a snippet into a string with placeholders (used for subsequent insertions):
+
+```js
+var inject = new InjectString('before <!-- snippet --> after');
+inject.append('foo', {
+  stripTags: false
+});
+//=> 'before <!-- snippet -->\nfoo\n<!-- endsnippet --> after'
+```
+
+### Strip placeholders
+
+Inject a snippet into a string without placeholders:
+
+```js
+var inject = new InjectString('before <!-- snippet --> after');
+inject.append('foo', {
+  stripTags: true
+});
+//=> 'before foo after'
+```
+
+### Use a custom tag name
+
+Customize the placeholder name:
+
+```js
+var inject = new InjectString('before <!-- xyz --> after');
+inject.append(str, 'foo', {tag: 'xyz'})
+//=> 'before foo after'
+```
+
+
+### Use custom delimiters
+
+Customize the placeholder delimiters:
+
+```js
+var str = new InjectString('a {{!snippet}} b', {delimiters: ['{{!', '}}']});
+var result = inject.append('foo');
+//=> 'a {{! snippet }}foo{{! endsnippet }} b'
+```
+
+### Add newlines
+
+```js
+var inject = new InjectString('a <!-- snippet --> b', {newlines: true});
+var result = inject.append('foo');
+//=> 'a\n<!-- snippet -->\nfoo\n<!-- endsnippet -->\nb'
+```
+
+## Running tests
+
+Install dev dependencies:
+
+```sh
+$ npm install -d && npm test
+```
+
+## Contributing
+
+Pull requests and stars are always welcome. For bugs and feature requests, [please create an issue](https://github.com/amazingsurge/inject-string/issues/new)
+
+## Credits
+This library is based on [inject-snippet](https://github.com/jonschlinkert/inject-snippet).
+
 ## License
 
-MIT © [amazingSurge]()
-
+MIT © [amazingSurge](amazingsurge.com)
 
 [npm-image]: https://badge.fury.io/js/inject-string.svg
 [npm-url]: https://npmjs.org/package/inject-string
--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
 import {expect} from 'chai';
-import Inject from '../lib/';
+import InjectString from '../lib/';
 
-describe('inject snippet', () => {
-  describe('Inject()', () => {
+describe('InjectString', () => {
+  describe('constructor()', () => {
     it('should exists:', () => {
-      const inject = new Inject('Hello world');
+      const inject = new InjectString('Hello world');
       expect(inject.toString()).to.equal('Hello world');
     });
+  });
 
+  describe('toString()', () => {
     it('should use the updated string after inject:', () => {
-      const inject = new Inject('a <!-- snippet --> b');
+      const inject = new InjectString('a <!-- snippet --> b');
       const result = inject.append('foo');
       expect(inject.toString()).to.equal(result);
     });
@@ -18,32 +20,32 @@ describe('inject snippet', () => {
   describe('errors', () => {
     it('should throw an error when invalid arguments are passed:', () => {
       expect(() => {
-        Inject.inject();
+        InjectString.inject();
       }).to.throw(/expected a string as the first argument./);
 
       expect(() => {
         // eslint-disable-next-line no-new
-        new Inject();
+        new InjectString();
       }).to.throw(/expected a string as the first argument./);
     });
   });
 
-  describe('inject', () => {
+  describe('inject()', () => {
     it('should inject a snippet into a string with one placeholder:', () => {
-      const inject = new Inject('a <!-- snippet --> b');
+      const inject = new InjectString('a <!-- snippet --> b');
       const result = inject.append('foo');
 
       expect(result).to.equal('a <!-- snippet -->foo<!-- endsnippet --> b');
     });
 
     it('should inject a snippet into a string with before/after placeholders:', () => {
-      const inject = new Inject('a <!-- snippet -->\nfoo\n<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->\nfoo\n<!-- endsnippet --> b');
       const result = inject.append('bar\n');
       expect(result).to.equal('a <!-- snippet -->\nfoo\nbar\n<!-- endsnippet --> b');
     });
 
     it('should inject snippets into a string with one placeholder:', () => {
-      const inject = new Inject('a <!-- snippet --> b');
+      const inject = new InjectString('a <!-- snippet --> b');
       const result = inject.append('foo');
 
       expect(result).to.equal('a <!-- snippet -->foo<!-- endsnippet --> b');
@@ -56,7 +58,7 @@ describe('inject snippet', () => {
     });
 
     it('should inject snippets into a string with different placeholders:', () => {
-      const inject = new Inject('a <!-- a --> b <!-- b --> c');
+      const inject = new InjectString('a <!-- a --> b <!-- b --> c');
       const result = inject.append('foo', 'a');
 
       expect(result).to.equal('a <!-- a -->foo<!-- enda --> b <!-- b --> c');
@@ -68,90 +70,115 @@ describe('inject snippet', () => {
 
   describe('actions', () => {
     it('should append a snippet into a string:', () => {
-      const inject = new Inject('a <!-- snippet -->foo<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->foo<!-- endsnippet --> b');
       const result = inject.append('bar');
       expect(result).to.equal('a <!-- snippet -->foobar<!-- endsnippet --> b');
     });
 
     it('should prepend a snippet into a string:', () => {
-      const inject = new Inject('a <!-- snippet -->foo<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->foo<!-- endsnippet --> b');
       const result = inject.prepend('bar');
       expect(result).to.equal('a <!-- snippet -->barfoo<!-- endsnippet --> b');
     });
 
     it('should replace a snippet into a string:', () => {
-      const inject = new Inject('a <!-- snippet -->foo<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->foo<!-- endsnippet --> b');
       const result = inject.replace('bar');
       expect(result).to.equal('a <!-- snippet -->bar<!-- endsnippet --> b');
     });
   });
 
-  describe('strip', () => {
+  describe('strip()', () => {
     it('should strips with one placeholder:', () => {
-      const inject = new Inject('a <!-- snippet --> b');
+      const inject = new InjectString('a <!-- snippet --> b');
       expect(inject.strip()).to.equal('a  b');
     });
 
     it('should strips with empty before/after placeholders:', () => {
-      const inject = new Inject('a <!-- snippet --><!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet --><!-- endsnippet --> b');
       expect(inject.strip()).to.equal('a  b');
     });
 
     it('should strips with filled before/after placeholders:', () => {
-      const inject = new Inject('a <!-- snippet -->foo<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->foo<!-- endsnippet --> b');
       expect(inject.strip()).to.equal('a foo b');
     });
 
     it('should strips with custom before/after placeholders:', () => {
-      const inject = new Inject('a <!-- custom -->foo<!-- endcustom --> b');
+      const inject = new InjectString('a <!-- custom -->foo<!-- endcustom --> b');
       expect(inject.strip('custom')).to.equal('a foo b');
     });
   });
 
-  describe('stripAll', () => {
+  describe('stripAll()', () => {
     it('should strips all with one placeholder:', () => {
-      const inject = new Inject('a <!-- snippet --> b');
+      const inject = new InjectString('a <!-- snippet --> b');
       expect(inject.stripAll()).to.equal('a  b');
     });
 
     it('should strips all with empty before/after placeholders:', () => {
-      const inject = new Inject('a <!-- snippet --><!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet --><!-- endsnippet --> b');
       expect(inject.stripAll()).to.equal('a  b');
     });
 
     it('should strips all with filled before/after placeholders:', () => {
-      const inject = new Inject('a <!-- snippet -->foo<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- snippet -->foo<!-- endsnippet --> b');
       expect(inject.stripAll()).to.equal('a foo b');
     });
 
     it('should strips all with custom before/after placeholders:', () => {
-      const inject = new Inject('a <!-- custom -->foo<!-- endcustom --> b');
+      const inject = new InjectString('a <!-- custom -->foo<!-- endcustom --> b');
       expect(inject.stripAll()).to.equal('a foo b');
     });
 
     it('should strips all with different placeholders:', () => {
-      const inject = new Inject('a <!-- custom -->foo<!-- endcustom --> <!-- snippet -->bar<!-- endsnippet --> b');
+      const inject = new InjectString('a <!-- custom -->foo<!-- endcustom --> <!-- snippet -->bar<!-- endsnippet --> b');
       expect(inject.stripAll()).to.equal('a foo bar b');
     });
   });
 
   describe('options', () => {
     it('should inject a snippet into a string without placeholders:', () => {
-      const inject = new Inject('a <!-- snippet --> b', {stripTags: true});
+      const inject = new InjectString('a <!-- snippet --> b', {stripTags: true});
       const result = inject.append('foo');
       expect(result).to.equal('a foo b');
     });
 
     it('should add normalized newlines around snippets:', () => {
-      const inject = new Inject('a <!-- snippet --> b', {newlines: true});
+      const inject = new InjectString('a <!-- snippet --> b', {newlines: true});
       const result = inject.append('foo');
       expect(result).to.equal('a\n<!-- snippet -->\nfoo\n<!-- endsnippet -->\nb');
     });
 
     it('should use custom delimiters:', () => {
-      const inject = new Inject('a {{!snippet}} b', {delimiters: ['{{!', '}}']});
+      const inject = new InjectString('a {{!snippet}} b', {delimiters: ['{{!', '}}']});
       const result = inject.append('foo');
       expect(result).to.equal('a {{! snippet }}foo{{! endsnippet }} b');
+    });
+  });
+
+
+  describe('static methods', () => {
+    it('should inject snippet into a string with placeholder:', () => {
+      const result = InjectString.inject('a <!-- snippet --> b', 'foo');
+
+      expect(result).to.equal('a <!-- snippet -->foo<!-- endsnippet --> b');
+
+      const result2 = InjectString.inject('a <!-- snippet --> b', 'foo', {stripTags: true});
+
+      expect(result2).to.equal('a foo b');
+    });
+
+    it('should strips with placeholder:', () => {
+      const result = InjectString.strip('a <!-- snippet --><!-- custom --> b', {
+        tag: 'custom',
+      });
+      expect(result).to.equal('a <!-- snippet --> b');
+    });
+
+    it('should strips all with placeholder:', () => {
+      const result = InjectString.stripAll('a <!-- snippet --><!-- custom --> b');
+      expect(result).to.equal('a  b');
     });
   });
 });
